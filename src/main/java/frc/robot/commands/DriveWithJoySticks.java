6e6fafbf93c5bc32f98d5df7061e2ccbad33225e
phillipvolkov2 <95985990+phillipvolkov2@users.noreply.github.com>
2023-01-19 15:59:41 -0800

    Adjusted rotation speed

--- src/main/java/frc/robot/commands/DriveWithJoySticks.java
+++ src/main/java/frc/robot/commands/DriveWithJoySticks.java
@@ -32,7 +32,7 @@ public class DriveWithJoySticks extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    westCoastDrive.drive(transJoyStick.getY(), rotJoyStick.getX());
+    westCoastDrive.drive(rotJoyStick.getX()/1.5,transJoyStick.getY());
   }
 
   // Called once the command ends or is interrupted.
